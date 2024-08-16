cc561e77dc4f83ad99b30a83c470e2f6083c71c9
saha-sagnik <sagnik12347@gmail.com>
2024-08-16 23:35:41 +0530

    created the landing page skeleton using routes and pages

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return ( 
+        <footer className="hidden lg:block h-29 w-full 
+        border-t-2 border-slate-200 p-2">
+            Footer
+        </footer>
+     );
+}
+ 
+export default Footer;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-     <Button>Click me</Button>
+   This is a marketing page
     </div>
   );
 }

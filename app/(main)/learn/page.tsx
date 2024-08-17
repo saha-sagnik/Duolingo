eac72baef707c079988f732018975360acba5ee8
saha-sagnik <sagnik12347@gmail.com>
2024-08-18 01:49:32 +0530

    learn page wrappers added

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,25 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+import Header from "./header";
+import UserpProgress from "@/components/UserProgress";
+
 const Learn = () => {
     return ( 
-        <div>
-        Learn Page
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+       <StickyWrapper>
+       <UserpProgress
+       activeCourse={{title:"Spanish", imageSrc:"/es.svg"}}
+       hearts = {5}
+       points = {100}
+       hasActiveSubscription={false}
+       
+       />
+       </StickyWrapper>
+      
+       <FeedWrapper>
+       <Header title="Spanish"/>
+       </FeedWrapper>
+       
         </div>
      );
 }

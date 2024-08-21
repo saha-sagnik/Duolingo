d2c6a88aa1c6e6f594193cd927a133664ca709d6
saha-sagnik <sagnik12347@gmail.com>
2024-08-21 12:48:54 +0530

    schema and tables updated

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -17,6 +17,12 @@ const main = async () =>{
 
         await db.delete(schema.courses);
         await db.delete(schema.userProgress);
+        await db.delete(schema.units);
+        await db.delete(schema.lessons);
+        await db.delete(schema.challenges);
+        await db.delete(schema.challengeOptions);
+        await db.delete(schema.challengeProgress);
+
 
         await db.insert(schema.courses).values([
             {
@@ -35,6 +41,70 @@ const main = async () =>{
                 imageSrc: "/jp.svg"
             }
         ])
+
+        await db.insert(schema.units).values([
+            {
+                id: 1,
+                courseId: 1,
+                title: "Unit 1",
+                description: "Learn the basics of Spanish",
+                order: 1,
+            }
+        ]);
+
+        await db.insert(schema.lessons).values([
+            {
+                id:1,
+                unitId: 1,
+                order:  1,
+                title: "Nouns",
+            },
+            {
+                id: 2,
+                unitId: 1,
+                order:  2,
+                title: "Verbs",
+            },
+        ]);
+
+        await db.insert(schema.challenges).values([
+            {
+                id:1,
+                lessonId:1,
+                type: "SELECT",
+                order: 1,
+                question: 'Which one of these is the "the man"?',
+            },
+            
+        ]);
+
+        await db.insert(schema.challengeOptions).values([
+            {
+              id: 1,
+              challengeId: 1,
+              imageSrc: "/man.svg",
+              correct: true,  // Changed to boolean
+              text: "el hombre",
+              audioSrc: "/es_man.mp3",
+            },
+            {
+              id: 2,
+              challengeId: 1,
+              imageSrc: "/woman.svg",
+              correct: false,  // Changed to boolean
+              text: "la mujer",
+              audioSrc: "/es_woman.mp3",
+            },
+            {
+                id: 3,
+                challengeId: 1,
+                imageSrc: "/robot.svg",
+                correct: false,  // Changed to boolean
+                text: "el robot",
+                audioSrc: "/es_robot.mp3",
+              },
+          ]);
+          
 
         console.log("Seeding finished");
     } catch (error) {

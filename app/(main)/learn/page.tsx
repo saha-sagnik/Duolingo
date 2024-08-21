ee051887ae7628609be43c865339e8abea6ceb05
saha-sagnik <sagnik12347@gmail.com>
2024-08-21 11:12:11 +0530

    seed script

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -6,34 +6,28 @@ import { getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 
 const Learn = async () => {
+  const userProgressData = getUserProgress();
 
-    const userProgressData = getUserProgress();
+  const [userProgress] = await Promise.all([userProgressData]);
 
-    const [userProgress] = await Promise.all([
-        userProgressData
-    ]);
+  if (!userProgress || !userProgress.activeCourse) redirect("/courses");
 
-    if(!userProgress || !userProgress.activeCourse)
-        redirect("/courses");
+  return (
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserpProgress
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
 
-    return ( 
-        <div className="flex flex-row-reverse gap-[48px] px-6">
-       <StickyWrapper>
-       <UserpProgress
-       activeCourse={{title:"Spanish", imageSrc:"/es.svg"}}
-       hearts = {5}
-       points = {100}
-       hasActiveSubscription={false}
-       
-       />
-       </StickyWrapper>
-      
-       <FeedWrapper>
-       <Header title="Spanish"/>
-       </FeedWrapper>
-       
-        </div>
-     );
-}
- 
+      <FeedWrapper>
+        <Header title={userProgress.activeCourse.title} />
+      </FeedWrapper>
+    </div>
+  );
+};
+
 export default Learn;
